f922ba9dcc515cabc12412cf90212a9959d76a89
sanya13091984 <flaaa965@gmail.com>
2020-03-10 09:04:09 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Son son = new Son();
+        son.pechat("udacshi");
+
+    }
+}
---
+++ src/Son.java
@@ -0,0 +1,7 @@
+public class Son {
+
+    void pechat(String ha){
+        System.out.println();
+
+    }
+}
