14e061355877941eeaa56ca36ff3a99c7ef7cd0b
sanya13091984 <flaaa965@gmail.com>
2020-03-10 13:08:37 +0300

    /11

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args) {
     //333
         Son son = new Son();
-        son.pechat("udai");
-            
+        son.pechat("udai");//11
+        //22
+
     }
 }
